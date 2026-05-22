15b6ddfb880b9588322ebce0d8e0b917f1f4bdcd
ENS999 <enssne1223@gmail.com>
2026-05-22 15:15:53 +0800

    update database connection

--- database.py
+++ database.py
@@ -6,14 +6,18 @@ load_dotenv()
 
 class Database():
     def __init__(self):
-        self.connection = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            sslmode=os.getenv("DB_SSLMODE", "prefer")
-        )
+        database_url = os.getenv("DATABASE_URL")
+        if database_url:
+            self.connection = psycopg2.connect(database_url, sslmode="require")
+        else:
+            self.connection = psycopg2.connect(
+                host=os.getenv("DB_HOST"),
+                port=os.getenv("DB_PORT"),
+                dbname=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                sslmode=os.getenv("DB_SSLMODE", "prefer")
+            )
 
     def get_cursor(self):
         return self.connection.cursor()
